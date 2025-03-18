9d56138eb989516d64106670fa94206fe6cc092d
A7medAref <ahmedmostafa992001@gmail.com>
2025-03-18 14:17:38 +0200

    removing unused variable

--- src/private-server/private-server.service.ts
+++ src/private-server/private-server.service.ts
@@ -1,23 +1,18 @@
-import { Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ICustodyKeyPairResponse, IGenerateKeyPairResponse } from 'rox-custody_common-modules/libs/interfaces/generate-ket-pair.interface';
 import {
   _EventPatterns,
   _MessagePatterns,
-  DBIdentifierRMQ,
 } from 'rox-custody_common-modules/libs/utils/microservice-constants';
-import { firstValueFrom, Observable, Subject } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { GenerateKeyPairBridge } from 'rox-custody_common-modules/libs/interfaces/generate-key.interface';
 import { PrivateServerQueue } from 'src/libs/rmq/private-server.decorator';
 import { TenantService } from 'src/libs/decorators/tenant-service.decorator';
-import { mobileKey } from 'rox-custody_common-modules/libs/interfaces/push-key-to-mobile.interface';
 import { BackupStorageIntegrationService } from 'src/backup-storage-integration/backup-storage-integration.service';
 import { IRequestDataFromApiApproval } from 'rox-custody_common-modules/libs/interfaces/send-to-backup-storage.interface';
 
 @TenantService()
 export class PrivateServerService {
-  private readonly keyUpdatesSubject = new Subject<mobileKey>();
-
   constructor(
     @PrivateServerQueue() private readonly privateServerQueue: ClientProxy,
     private readonly backupStorageIntegrationService: BackupStorageIntegrationService
