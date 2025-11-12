8fce474389e23dee95ccaffffe9beafc03c4f865
Alaa Eldeen <alaaamin706@gmail.com>
2025-11-12 09:48:25 +0200

    support multiple email providers, with fall back

--- src/mail/options/strategies/verify-key.strategy.ts
+++ src/mail/options/strategies/verify-key.strategy.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@nestjs/common";
-import { IMailOptionsStrategy, IPartialMailOptions } from "../interfaces/mails-options-strategy.interface";
 import { readFile } from 'fs/promises';
 import { join } from "path";
-import { MailStrategy } from "../enums/mail-strategy.enum";
+import { MailStrategy } from "../../enums/mail-strategy.enum";
 import { VerifyKeyEmailStrategyPayload } from "src/libs/dto/verify-key-email-strategy-payload.dto";
+import { IMailOptionsStrategy, IPartialMailOptions } from "../interfaces/mails-options-strategy.interface";
 
 @Injectable()
 export class VerifyKeyMailOptionsStrategy implements IMailOptionsStrategy<MailStrategy.VERIFY_KEY> {
     async getMailOptions(payload: VerifyKeyEmailStrategyPayload): Promise<IPartialMailOptions> {
         const verifyKeyTemplate = await readFile(
-            join(__dirname, `./templates/backup-storage-verify-key.template.html`),
+            join(__dirname, `../../templates/backup-storage-verify-key.template.html`),
             { encoding: 'utf-8' },
         );
 
